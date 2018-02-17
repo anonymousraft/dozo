707f9ea642e8547a2e9a87d602bdd8f89a965a64
Hitendra Singh Rathore <hitendra1995@gmail.com>
2018-02-17 12:06:30 +0530

    Added bootstrape CSS and js. also configured meta view port

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<?php wp_head(); ?>
 </head>
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 
 <?php get_header();?>
 
+<div class="container">
+  <div class="col-md-12">
+    <p align="center"><h1>bootstrap Added</h1></p>
+    <h1>WELCOME TO DOZO THEME</h1>
+    <p>THE WEB STARTUP</p>
+    <p>adding bootstrap</p>
+    <div/>
 
-<h1>WELCOME TO DOZO THEME</h1>
-<p>THE WEB STARTUP</p>
+</div>
 
 
 <?php  get_footer();?>
