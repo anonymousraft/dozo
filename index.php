c017051b42f13a9ca78d56b256bc9953289fb595
mandeepgthub <suggestions.live@gmail.com>
2018-02-17 13:12:10 +0530

    test line created to check css on redbox can be deleted or changed

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 
-<?php get_header();?>
 
 <div class="container">
   <div class="col-md-12">
-    <p align="center"><h1>bootstrap Added</h1></p>
-    <h1>WELCOME TO DOZO THEME</h1>
-    <p>THE WEB STARTUP</p>
-    <p>adding bootstrap</p>
-<div/></div>
+
+	   <?php get_header();?>
+		<?php wp_head(); ?>
+         <p align="center"><h1>bootstrap Added</h1></p>
+         <h1>WELCOME TO DOZO THEME</h1>
+         <p>THE WEB STARTUP</p>
+         <p>adding bootstrap</p>
+	
+   </div>	
+</div>
+
+<div class="redbox">
+	test line to be deleteed
+</div>
 
 
 <?php  get_footer();?>
