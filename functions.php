1843130032ad3ab08e107a7bec793b3034e8ec1c
Hitendra Singh Rathore <hitendra1995@gmail.com>
2018-02-19 10:10:34 +0530

    Testing packages

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function dozo_script_enqueue(){
 	wp_enqueue_script('customjs', get_template_directory_uri().'/js/dozoscripts.js', array(),'1.0.1',true);
 	wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
 
-}
+	}
 
 add_action('wp_enqueue_scripts', 'dozo_script_enqueue');
 
