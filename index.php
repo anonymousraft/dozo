24baa0a6282f6a7a1dc3cb5745ed52e0435bf171
Hitendra Singh Rathore <hitendra1995@gmail.com>
2018-02-20 14:28:46 +0530

    Removed unwanted string from index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <p>THE WEB STARTUP</p>
     <p>adding bootstrap</p>
 </div></div>
-<<<<<<< HEAD
 <div class="container">
   <div class="col-md-6">
     <h1 class=> <small>Hello</small></h1>
@@ -17,8 +16,4 @@
     <h1> <small>World</small></h1>
   </div>
 </div>
-=======
->>>>>>> 539e42c1f38207805a38e4f077981cae1029e96f
-
-
 <?php  get_footer();?>
