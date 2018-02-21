aa02426cce158098d2960587f12aef5c464dd8d1
anonymousraft <hitendra1995@gmail.com>
2018-02-22 01:06:46 +0530

    Removed the <?php wp_head();?> added the blog function for showing category, date, author name, tags.

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
   <div>
     <div class="col-md-12">
 	       <?php get_header();?>
-		    <?php wp_head(); ?>
             <?php
               if( have_posts() ):
                 while( have_posts() ): the_post(); ?>
 
                     <h3><?php the_title(); ?></h3>
+                    <p>Category: <?php single_cat_title(); ?></p>
+                    <p>This article was published on: <?php the_time('m/j/y g:i A') ?></p>
+                    <p>This article was written by <?php the_author(); ?></p>
+                    <p>Tags: <?php the_tags(); ?></p>
                     <p><?php the_content(); ?></p>
 
               <?php  endwhile;
