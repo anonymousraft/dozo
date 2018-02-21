ef4ea224dbdbb54a893d166977689ab9faf1d4be
mandeephub <suggestions.live@gmail.com>
2018-02-21 23:26:00 +0530

    clearup the code

--- index.php
+++ index.php
@@ -4,35 +4,9 @@
 		<?php wp_head(); ?>
          <p align="center"><h1>bootstrap Added</h1></p>
          <h1>WELCOME TO DOZO THEME</h1>
-         <p>THE WEB STARTUP</p>
-         <p>THE WEB STARTUP atom </p>
-         <p>adding bootstrap</p>
-
-   </div>
-</div>
-<div class="redbox">
-	test line to be deleteed
-</div>
-
-    <p align="center"><h1>bootstrap Added</h1></p>
-    <h1>WELCOME TO DOZO THEME</h1>
-    <p>THE WEB STARTUP</p>
-    <p>adding bootstrap</p>
-</div></div>
-
-<div class="container">
-  <div class="col-md-6">
-    <h1 class=> <small>Hello</small></h1>
-  </div>
-  <div class="col-md-6">
-    <h1> <small>World</small></h1>
+        
   </div>
 </div>
-=======
->>>>>>> 539e42c1f38207805a38e4f077981cae1029e96f
->>>>>>> 33f35a8ea420868d9228463756ada0ff67c1592a
 
 
-=======
->>>>>>> 311db4d64b501d48e1f6107b866790edada50535
 <?php  get_footer();?>
